ea6ef18a87558818a614ba08caa8b5c1b16539c3
Yevgeny Levin <devlev1980@gmail.com>
2021-07-10 12:04:44 +0300

    add project

---
+++ data.js
@@ -0,0 +1,24 @@
+let students = [
+    {
+        id: 0,
+        name: 'Igor',
+        age: 20,
+        phone: 123,
+        isFavorite: false
+    },
+    {
+        id: 1,
+        name: 'Alex',
+        age: 21,
+        phone: 345,
+        isFavorite: true
+    },
+    {
+        id: 2,
+        name: 'Misha',
+        age: 22,
+        phone: 567,
+        isFavorite: false
+    },
+
+]
